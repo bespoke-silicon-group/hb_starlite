86f2bd0c3c960f4a37d6919bcf61cfe1319d5edb
Adrian Sampson <adrian@radbox.org>
2019-05-27 14:01:57 -0400

    Switch to modified compile function
    We need to control the name of the module.

--- py-graphit-example/graphit_util.py
+++ py-graphit-example/graphit_util.py
@@ -1,7 +1,51 @@
 import graphit
 import os
-import shutil
 import importlib
+import subprocess
+import platform
+
+
+def _compile(graphit_source_file, module_name, module_file):
+    base, _ = os.path.splitext(module_file)
+    module_filename_cpp = '{}.cpp'.format(base)
+    module_filename_object = '{}.o'.format(base)
+
+    subprocess.check_call(
+        "python " + graphit.GRAPHIT_BUILD_DIRECTORY +
+        "/bin/graphitc.py -f " + graphit_source_file + " -o " +
+        module_filename_cpp + " -m " + module_name,
+        shell=True,
+    )
+
+    compile_command = (
+        graphit.CXX_COMPILER + " -I" +
+        graphit.pybind11.get_include(True) +
+        " $(python3-config --includes) -c -I " +
+        graphit.GRAPHIT_SOURCE_DIRECTORY +
+        "/src/runtime_lib/ -std=c++11 -DGEN_PYBIND_WRAPPERS -flto "
+        "-fno-fat-lto-objects -fPIC -fvisibility=hidden "
+    )
+
+    subprocess.check_call(
+        compile_command + module_filename_cpp +
+        " -o " + module_filename_object,
+        shell=True,
+    )
+
+    cmd = (
+        graphit.CXX_COMPILER + " -fPIC -shared -o " + module_file +
+        " " + module_filename_object + " -flto"
+    )
+
+    if platform.system() == "Darwin":
+        cmd = cmd + "-undefined dynamic_lookup"
+
+    subprocess.check_call(cmd, shell=True)
+    spec = importlib.util.spec_from_file_location(module_name, module_file)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    os.unlink(module_filename_cpp)
+    os.unlink(module_filename_object)
 
 
 def load_cached(graphit_filename, lib_filename=None):
@@ -26,10 +70,7 @@ def load_cached(graphit_filename, lib_filename=None):
     # if necessary.
     if not os.path.exists(lib_filename) or \
        os.stat(graphit_filename).mtime > os.stat(lib_filename):
-        # By default, the GraphIt module compiles to /tmp. We'll do
-        # that, then copy the underlying file to our intended location.
-        mod = graphit.compile_and_load(graphit_filename)
-        shutil.copyfile(mod.__file__, lib_filename)
+        _compile(graphit_filename, mod_name, lib_filename)
 
     # (Re)load the module.
     spec = importlib.util.spec_from_file_location(mod_name, lib_filename)
