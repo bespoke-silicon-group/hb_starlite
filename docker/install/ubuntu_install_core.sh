5963cf28b1ef082d2fffff6fb2a9cb6c300fa6e9
Adrian Sampson <adrian@radbox.org>
2020-06-19 11:13:11 -0400

    More prompt suppression

--- docker/install/ubuntu_install_core.sh
+++ docker/install/ubuntu_install_core.sh
@@ -21,8 +21,9 @@ set -u
 set -o pipefail
 
 # install libraries for building c++ core on ubuntu
-apt-get update && apt-get install -y --no-install-recommends \
-        git make libgtest-dev cmake wget unzip libtinfo-dev libz-dev\
+apt-get update && DEBIAN_FRONTEND=noninteractive \
+        apt-get install -y --no-install-recommends \
+        git make libgtest-dev cmake wget unzip libtinfo-dev libz-dev \
         libcurl4-openssl-dev libopenblas-dev g++ sudo
 
 cd /usr/src/gtest && cmake CMakeLists.txt && make && cp *.a /usr/lib
